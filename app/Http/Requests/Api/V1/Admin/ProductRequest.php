d3e98708dc4de1f4a0ee566938fbcdf91e253082
Naveigell <yosua.yerikho123@gmail.com>
2022-03-12 22:23:20 +0800

    crud product

--- app/Http/Requests/Api/V1/Admin/ProductRequest.php
+++ app/Http/Requests/Api/V1/Admin/ProductRequest.php
@@ -14,7 +14,13 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "image"          => "required|image|mimes:jpg,png,jpeg",
+            "name"           => "required|string|min:2|max:255",
+            "description"    => "required|string|min:5|max:10000",
+            "price"          => "required|integer|min:1|max:1000000000", // 1 million
+            "stock"          => "required|integer|min:|max:9999",
+            "release_date"   => "nullable|date",
+            "estimated_date" => "nullable|date",
         ];
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @method static|\Illuminate\Database\Eloquent\Builder inRandomOrder($seed = '')
@@ -13,10 +15,32 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'image', 'name', 'description', 'price', 'stock', 'release_date', 'estimated_date',
+    ];
+
     protected $appends = ['image_url'];
 
     public function getImageUrlAttribute()
     {
         return asset('storage/images/products/' . $this->attributes['image']);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = \Str::slug($value) . '-' . ($this->id ?? rand(1, 10));
+    }
+
+    /**
+     * @param UploadedFile $file
+     */
+    public function setImageAttribute($file)
+    {
+        $name = \Str::random(30) . uniqid() . date('dmYHis') . '.' . $file->getClientOriginalExtension();
+
+        Storage::putFileAs('public/images/products', $file, $name);
+
+        $this->attributes['image'] = $name;
     }
 }
