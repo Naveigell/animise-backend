ad56f16daf58d822d35df139053c0105013f6bd9
Naveigell <yosua.yerikho123@gmail.com>
2022-03-13 11:44:29 +0800

    add product search

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @method static|\Illuminate\Database\Eloquent\Builder inRandomOrder($seed = '')
- * @method static|\Illuminate\Database\Eloquent\Builder find($id, $columns = ['*'])
+ * @method \Illuminate\Database\Eloquent\Builder|static inRandomOrder($seed = '')
+ * @method \Illuminate\Database\Eloquent\Builder|static find($id, $columns = ['*'])
+ * @method \Illuminate\Database\Eloquent\Builder|static search(string $value)
+ * @method \Illuminate\Database\Eloquent\Builder|static stockAvailable()
  */
 class Product extends Model
 {
@@ -18,5 +21,24 @@ class Product extends Model
     public function getImageUrlAttribute()
     {
         return asset('storage/images/products/' . $this->attributes['image']);
+    }
+
+    /**
+     * @param Builder $query
+     * @param $value
+     * @return Builder
+     */
+    public function scopeSearch($query, $value)
+    {
+        return $query->where('name', 'LIKE', "%{$value}%");
+    }
+
+    /**
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeStockAvailable($query)
+    {
+        return $query->where('stock', '>', 0);
     }
 }
