31d718da0ac5f289785428d1e5a7a0914c472d14
Naveigell <yosua.yerikho123@gmail.com>
2022-07-29 21:22:21 +0800

    init payment

---
+++ app/Http/Requests/Api/V1/User/PaymentRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "image" => "required|image|mimes:png,jpg,jpeg"
+        ];
+    }
+}
---
+++ database/migrations/2022_07_29_130339_create_payment_product_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentProductOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_product_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('product_order_id')->constrained('product_orders')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_product_order');
+    }
+}
