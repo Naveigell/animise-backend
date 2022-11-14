60414aac57468070efa9e69166a4b9f578f5fb23
Naveigell <yosua.yerikho123@gmail.com>
2022-11-14 22:23:11 +0800

    merge conflict

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -7,10 +7,7 @@ use App\Models\Product;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Http\UploadedFile;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\File;
->>>>>>> feature/user/banner
 
 class ProductSeeder extends Seeder
 {
@@ -34,18 +31,12 @@ class ProductSeeder extends Seeder
 
         File::ensureDirectoryExists(storage_path('app/public/images/products'));
 
-<<<<<<< HEAD
-            Product::create([
-                "category_id"  => $categories[array_rand($categories)],
-                "image"        => UploadedFile::fake()->image(\Str::random(20) . '.jpg'),
-=======
         for ($i = 0; $i < 30; $i++) {
             $name = $faker->realTextBetween(5, 10) . uniqid();
 
             Product::query()->create([
                 "category_id"  => $categories[array_rand($categories)],
                 "image"        => UploadedFile::fake()->image(\Str::random() . '.jpg'),
->>>>>>> feature/user/banner
                 "name"         => $name,
                 "slug"         => \Str::slug($name),
                 "description"  => $this->description(),
