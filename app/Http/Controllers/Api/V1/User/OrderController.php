d457676a843d0c49acc6e088afe272261ae7ddc6
Naveigell <yosua.yerikho123@gmail.com>
2022-11-23 21:12:36 +0800

    change order response

--- app/Http/Controllers/Api/V1/User/OrderController.php
+++ app/Http/Controllers/Api/V1/User/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1\User;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\User\Order\OrderCollection;
 use App\Models\ProductOrder;
+use App\Models\Shipping;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -16,8 +17,8 @@ class OrderController extends Controller
      */
     public function index(Request $request)
     {
-        $orders = ProductOrder::with('product.category', 'shipping')->where('user_id', $request->user()->id)->get();
+        $shippings = Shipping::with('productOrders.product', 'payments', 'user.biodata')->where('user_id', $request->user()->id)->get();
 
-        return new OrderCollection($orders);
+        return new OrderCollection($shippings);
     }
 }
